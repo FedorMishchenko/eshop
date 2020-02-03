620afdc51a987b1e53c6cd20055f7ebd5d0f2ba8
Rodyslav Sinitsyn <Rodyslav_Sinitsyn@epam.com>
2020-02-03 18:57:02 +0200

    ES-4
    Fix UUID and properties.

--- src/main/java/com/epam/eshop/domain/AbstractEntity.java
+++ src/main/java/com/epam/eshop/domain/AbstractEntity.java
@@ -13,6 +13,6 @@ import java.util.UUID;
 public class AbstractEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 }
